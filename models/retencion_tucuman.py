94025b60fd896a38f5e48ddd10a843fb842d7d69
Gonzalo Zelaya <gonzalo.zelaya@outsourcearg.com>
2025-06-19 16:52:47 -0300

    Updated module

--- models/retencion_tucuman.py
+++ models/retencion_tucuman.py
@@ -25,12 +25,12 @@ class AccountPayment(models.Model):
     tucuman_alicuota = fields.Float('Alicuota')
     tucuman_cf = fields.Float('Coeficiente', digits=(16, 4),default = 0.0447)
     tucuman_c_m = fields.Float('C.M.',default=0.5)
-    es_retencion_tucuman = fields.Boolean('Renteción Tucumán',related ='partner_id.x_studio_retencin_tucuman')
+    es_retencion_tucuman = fields.Boolean('Renteción Tucumán',related ='partner_id.x_studio_retencion_tucuman')
 
 class PaymentGroup(models.Model):
     _inherit = 'account.payment.multiplemethods'
     tucuman_alicuota = fields.Float('Alicuota')
     tucuman_cf = fields.Float('Coeficiente', digits=(16, 4),default = 0.0447)
     tucuman_c_m = fields.Float('C.M.',default=0.5)
-    es_retencion_tucuman = fields.Boolean('Renteción Tucumán',related ='partner_id.x_studio_retencin_tucuman')
+    es_retencion_tucuman = fields.Boolean('Renteción Tucumán',related ='partner_id.x_studio_retencion_tucuman')
  
