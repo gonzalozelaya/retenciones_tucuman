4ceb57f03c909feddb0028d019cc84983bc01000
gonzalozelaya <gonzalo.zelaya@outsourcearg.com>
2025-06-28 22:16:45 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,2 @@
 # -*- coding: utf-8 -*-
-
-from . import res_currency
-from . import account_move_line
-from . import account_move
-from . import sale_make_invoice
 from . import retencion_tucuman
-from. import reporte_balance
